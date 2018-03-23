25235978f82a17e37b6d5ae7faf347908b1599d6
paveena <p.p.073125@gmail.com>
2018-03-23 11:50:11 +0700

    Toolbar Noti Ok1

--- app/src/main/java/masterung/androidthai/in/th/ungreadcode/fragment/ShowNotiFrament.java
+++ app/src/main/java/masterung/androidthai/in/th/ungreadcode/fragment/ShowNotiFrament.java
@@ -1,11 +1,14 @@
 package masterung.androidthai.in.th.ungreadcode.fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.Toolbar;
 
 import masterung.androidthai.in.th.ungreadcode.NotificationActivity;
@@ -37,12 +40,28 @@ public class ShowNotiFrament extends Fragment{
 //        Get Value From Argument
         messageStrings = getArguments().getStringArray("Message");
 
+//        Get Vaule From SherePreferance
+        getValveFromContext();
+
+
 //        Create Toolbar
         createToolbar();
 
+//        Creat ListView
+
+        creatListView();
 
 
     } // Main Class
+
+    private void getValveFromContext() {
+        SharedPreferences sharedPreferences = getActivity()
+                .getSharedPreferences("LonginFile", Context.MODE_PRIVATE);
+    }
+
+    private void creatListView() {
+        ListView listView = getView().findViewById(R.id.listViewMessage);
+    }
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarShowNoti);
